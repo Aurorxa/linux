6bfdb0ece2e6c1be808fd9400f3b7d005c2c5f26
许大仙 <1900919313@qq.com>
2024-12-02 10:45:54 +0800

    初始化

--- docs/.vitepress/navbar/index.ts
+++ docs/.vitepress/navbar/index.ts
@@ -2,5 +2,6 @@ import { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.NavItem[] = [
   { text: '🏠首页', link: '/' },
   { text: '🎉快速开始', link: '/notes/' },
+  { text: '🍉主站', link: 'https://weiweixu.cn' },
   { text: '🌟关于我', link: '/notes/about/' },
 ]
